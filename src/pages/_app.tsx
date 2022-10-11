bad96ad17e6d88dee98c5c2ebfff53a6b969e15d
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-10-11 16:39:57 +0100

    Setup fontawesome icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from "next/app"
 
+import { config } from "@fortawesome/fontawesome-svg-core"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+
 import "../styles/global.css"
+
+config.autoAddCss = false
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />
