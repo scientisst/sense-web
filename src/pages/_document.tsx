021987f0040dc106265fc26374f2a06c08f16ceb
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-10-11 14:36:46 +0100

    Font optimization

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,20 @@ import { Head, Html, Main, NextScript } from "next/document"
 export default function Document() {
 	return (
 		<Html>
-			<Head />
+			<Head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;500;600;700;800;900&display=swap"
+					rel="stylesheet"
+				/>
+				<link
+					rel="preload"
+					href="/static/imagine.otf"
+					as="font"
+					type="font/opentype"
+					crossOrigin="anonymous"
+				/>
+				{/* <link href="/static/imagine.css" rel="stylesheet" /> */}
+			</Head>
 			<body className="bg-secondary">
 				<Main />
 				<NextScript />
