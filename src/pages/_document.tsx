962d06cbf00f78b3975b127168fe94d230f2238d
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-10-11 15:09:16 +0100

    remove zombie code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,14 +8,6 @@ export default function Document() {
 					href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;500;600;700;800;900&display=swap"
 					rel="stylesheet"
 				/>
-				<link
-					rel="preload"
-					href="/static/imagine.otf"
-					as="font"
-					type="font/opentype"
-					crossOrigin="anonymous"
-				/>
-				{/* <link href="/static/imagine.css" rel="stylesheet" /> */}
 			</Head>
 			<body className="bg-secondary">
 				<Main />
