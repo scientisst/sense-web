9a101749eb67a9e117505342538c28d85b8c5e40
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-10-12 13:21:38 +0100

    added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
 					href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;500;600;700;800;900&display=swap"
 					rel="stylesheet"
 				/>
+				<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
 			</Head>
 			<body className="bg-secondary text-base font-normal leading-6 text-primary-black">
 				<Main />
