e8df581e5dd2e9d460bf7e387a1ef281e738a2e8
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-10-12 14:05:00 +0100

    Landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,35 @@
+import Link from "next/link"
+
+import ScientISSTSENSE from "../assets/logos/ScientISSTSENSE.svg"
+import ScientISSTSENSEVertical from "../assets/logos/ScientISSTSENSEVertical.svg"
+import CenteredLayout from "../components/layout/CenteredLayout"
+
+interface NavButtonProps {
+	children?: React.ReactNode
+	href: React.ComponentPropsWithoutRef<typeof Link>["href"]
+}
+
+const NavButton: React.FC<NavButtonProps> = ({ children, href }) => {
+	return (
+		<Link href={href}>
+			<button className="flex flex-grow flex-col items-center rounded-lg bg-tint-red py-4 px-8 text-lg font-medium uppercase text-primary-white hover:bg-tint-red-dark">
+				{children}
+			</button>
+		</Link>
+	)
+}
+
 const Page = () => {
-	return <div>Welcome to Next.js!</div>
+	return (
+		<CenteredLayout className="flex w-[360px] flex-col items-center gap-12 px-4 py-8 sm:w-auto">
+			<ScientISSTSENSE className="hidden sm:block" />
+			<ScientISSTSENSEVertical className="sm:hidden" />
+			<div className="flex flex-col gap-4 self-stretch sm:flex-row">
+				<NavButton href="/live">Live</NavButton>
+				<NavButton href="/settings">Settings</NavButton>
+			</div>
+		</CenteredLayout>
+	)
 }
 
 export default Page
