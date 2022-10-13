b80d15df38a5111a50623d9b620f306de0235e28
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-10-13 17:07:04 +0100

    dynamic dark theme support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react"
+
 import type { AppProps } from "next/app"
 
 import { config } from "@fortawesome/fontawesome-svg-core"
@@ -8,5 +10,20 @@ import "../styles/global.css"
 config.autoAddCss = false
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+	useEffect(() => {
+		const currentColorScheme =
+			"theme" in localStorage
+				? localStorage.theme
+				: window.matchMedia("(prefers-color-scheme: dark)").matches
+				? "dark"
+				: "light"
+
+		if (currentColorScheme === "dark") {
+			document.documentElement.classList.add("dark")
+		} else {
+			document.documentElement.classList.remove("dark")
+		}
+	}, [])
+
 	return <Component {...pageProps} />
 }
