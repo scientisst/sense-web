798d98a958f8c622aa1ff2e428af5a0d5d893258
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-10-14 11:50:28 +0100

    replaced color map with tintBackgroundClass

--- src/components/inputs/TextButton.tsx
+++ src/components/inputs/TextButton.tsx
@@ -1,5 +1,5 @@
 import joinClassNames from "../utils/joinClassNames"
-import { TintColor } from "../utils/tints"
+import { TintColor, tintBackgroundClass } from "../utils/tints"
 
 export type TextButtonSize = "base" | "lg"
 
@@ -9,16 +9,6 @@ export interface TextButtonProps {
 	size: TextButtonSize
 	className?: string
 	style?: React.CSSProperties
-}
-
-const tintColorClasses: Record<TintColor, string> = {
-	yellow: "bg-tint-yellow dark:bg-tint-yellow-dark text-primary-black",
-	green: "bg-tint-green dark:bg-tint-green-dark text-primary-white",
-	mint: "bg-tint-mint dark:bg-tint-mint-dark text-primary-white",
-	blue: "bg-tint-blue dark:bg-tint-blue-dark text-primary-white",
-	purple: "bg-tint-purple dark:bg-tint-purple-dark text-primary-white",
-	red: "bg-tint-red dark:bg-tint-red-dark text-primary-white",
-	orange: "bg-tint-orange dark:bg-tint-orange-dark text-primary-white"
 }
 
 const sizeClasses: Record<TextButtonSize, string> = {
@@ -37,7 +27,7 @@ const TextButton: React.FC<TextButtonProps> = ({
 		<button
 			className={joinClassNames(
 				"flex items-center justify-center rounded-lg font-medium uppercase motion-safe:hover:scale-hover motion-safe:active:scale-pressed",
-				tintColorClasses[tint],
+				tintBackgroundClass[tint],
 				sizeClasses[size],
 				className
 			)}
