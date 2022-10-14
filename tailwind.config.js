37a1e6e6f163b879aceddfc30dd4ed9b2d9b51a0
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-10-14 17:40:14 +0100

    move all colors to global list

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,23 +18,19 @@ module.exports = {
 				"tint-red": "#EF4B59",
 				"tint-red-dark": "#F05463",
 				"tint-orange": "#F1643D",
-				"tint-orange-dark": "#F26C46"
-			},
-			backgroundColor: {
+				"tint-orange-dark": "#F26C46",
+				"primary-black": "rgba(0, 0, 0, 1.0)",
+				"primary-white": "rgba(255, 255, 255, 1.0)",
+				"secondary-black": "rgba(0, 0, 0, 0.8)",
+				"secondary-white": "rgba(255, 255, 255, 0.95)",
+				"tertiary-black": "rgba(60, 60, 67, 0.5)",
+				"tertiary-white": "#9CA3AF",
+				"quaternary-black": "rgba(60, 60, 67, 0.18)",
+				"quaternary-white": "rgba(235, 235, 245, 0.18)",
 				primary: "#FFFFFF",
 				"primary-dark": "#1C1C1E",
 				secondary: "#F2F2F7",
 				"secondary-dark": "#2C2C2E"
-			},
-			textColor: {
-				"primary-black": "rgba(0, 0, 0, 1.0)",
-				"primary-white": "rgba(255, 255, 255, 1.0)",
-				"secondary-black": "rgba(60, 60, 66, 0.6)",
-				"secondary-white": "rgba(235, 235, 245, 0.6)",
-				"tertiary-black": "rgba(60, 60, 67, 0.3)",
-				"tertiary-white": "rgba(235, 235, 245, 0.3)",
-				"quaternary-black": "rgba(60, 60, 67, 0.18)",
-				"quaternary-white": "rgba(235, 235, 245, 0.18)"
 			},
 			scale: {
 				hover: "1.1",
