acd66e86408da3072142727d48a6b5c205a01583
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-10-14 11:49:34 +0100

    Record to match tints to respective background

--- src/components/utils/tints.ts
+++ src/components/utils/tints.ts
@@ -6,3 +6,13 @@ export type TintColor =
 	| "purple"
 	| "red"
 	| "orange"
+
+export const tintBackgroundClass: Record<TintColor, string> = {
+	yellow: "bg-tint-yellow dark:bg-tint-yellow-dark text-primary-black",
+	green: "bg-tint-green dark:bg-tint-green-dark text-primary-white",
+	mint: "bg-tint-mint dark:bg-tint-mint-dark text-primary-white",
+	blue: "bg-tint-blue dark:bg-tint-blue-dark text-primary-white",
+	purple: "bg-tint-purple dark:bg-tint-purple-dark text-primary-white",
+	red: "bg-tint-red dark:bg-tint-red-dark text-primary-white",
+	orange: "bg-tint-orange dark:bg-tint-orange-dark text-primary-white"
+}
