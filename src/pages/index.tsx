a3526b8bb9b99b2d4bcac89778326e2ee458f425
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-10-17 14:55:47 +0100

    Tweak landing page spacing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,20 @@ import Link from "next/link"
 import ScientISSTSenseLogo from "../assets/logos/ScientISSTSenseLogo"
 import ScientISSTSenseVerticalLogo from "../assets/logos/ScientISSTSenseVerticalLogo"
 import TextButton from "../components/inputs/TextButton"
-import CenteredLayout from "../components/layout/CenteredLayout"
+import SenseLayout from "../components/layout/SenseLayout"
 
 const Page = () => {
 	return (
-		<CenteredLayout className="flex w-[360px] flex-col items-center gap-12 px-4 py-8 sm:w-auto">
-			<ScientISSTSenseLogo className="hidden sm:block" />
+		<SenseLayout
+			className="flex w-[480px] flex-col items-center justify-center gap-8 py-8 px-8 sm:w-[640px]"
+			header={false}
+			style={{
+				minHeight: "calc(100vh - 14.5rem)"
+			}}
+		>
+			<ScientISSTSenseLogo className="hidden h-auto w-full sm:block" />
 			<ScientISSTSenseVerticalLogo className="sm:hidden" />
-			<div className="flex flex-col gap-4 self-stretch sm:flex-row">
+			<div className="flex flex-col gap-8 self-stretch sm:flex-row">
 				<Link href="/live">
 					<TextButton tint="red" size="lg" className="flex-grow">
 						Live
@@ -22,7 +28,7 @@ const Page = () => {
 					</TextButton>
 				</Link>
 			</div>
-		</CenteredLayout>
+		</SenseLayout>
 	)
 }
 
