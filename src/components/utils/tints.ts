194134936be39f2c75ffaacabaa6b8e79e4d53b7
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-10-17 19:16:44 +0100

    tint border colors

--- src/components/utils/tints.ts
+++ src/components/utils/tints.ts
@@ -16,3 +16,13 @@ export const tintBackgroundClass: Record<TintColor, string> = {
 	red: "bg-tint-red dark:bg-tint-red-dark text-primary-white",
 	orange: "bg-tint-orange dark:bg-tint-orange-dark text-primary-white"
 }
+
+export const tintBorderClass: Record<TintColor, string> = {
+	yellow: "border-tint-yellow dark:border-tint-yellow-dark",
+	green: "border-tint-green dark:border-tint-green-dark",
+	mint: "border-tint-mint dark:border-tint-mint-dark",
+	blue: "border-tint-blue dark:border-tint-blue-dark",
+	purple: "border-tint-purple dark:border-tint-purple-dark",
+	red: "border-tint-red dark:border-tint-red-dark",
+	orange: "border-tint-orange dark:border-tint-orange-dark"
+}
