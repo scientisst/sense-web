50f714056b9660080a61b263077305e0eba68a5a
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-11-06 14:33:58 +0000

    remove redundant setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	presets: [require("@scientisst/react-ui/tailwindcss-config")],
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	darkMode: "class"
+	content: ["./src/**/*.{js,jsx,ts,tsx}"]
 }
