ae970f20c02adb60d826b39e0c9ccd8e48fea5ca
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-11-08 16:32:02 +0000

    add example for changing main colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	presets: [require("@scientisst/react-ui/tailwindcss-config")],
-	content: ["./src/**/*.{js,jsx,ts,tsx}"]
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				// "primary-light": "#5DAF4F",
+				// "primary-dark": "#5EBA4E"
+			},
+			fontFamily: {
+				// primary: ["Lexend", "sans-serif"],
+				// secondary: ["Imagine", "sans-serif"],
+			}
+		}
+	}
 }
