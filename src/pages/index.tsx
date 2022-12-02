492fb356b6af1f8011df1f5837b3d820c703d930
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-12-02 15:00:15 +0000

    remove minimum height for main content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,20 +9,18 @@ import SenseLayout from "../components/layout/SenseLayout"
 const Page = () => {
 	return (
 		<SenseLayout
+			title="Welcome!"
 			className="flex w-[480px] flex-col items-center justify-center gap-8 py-8 px-8 sm:w-[640px]"
-			style={{
-				minHeight: "calc(100vh - 18.5rem)"
-			}}
 		>
 			<ScientISSTSenseLogo className="hidden h-auto w-full sm:block" />
 			<ScientISSTSenseVerticalLogo className="sm:hidden" />
 			<div className="flex flex-col gap-8 self-stretch sm:flex-row">
-				<Link href="/live">
+				<Link href="/live" legacyBehavior>
 					<TextButton size="lg" className="flex-grow">
 						Live
 					</TextButton>
 				</Link>
-				<Link href="/settings">
+				<Link href="/settings" legacyBehavior>
 					<TextButton size="lg" className="flex-grow">
 						Settings
 					</TextButton>
