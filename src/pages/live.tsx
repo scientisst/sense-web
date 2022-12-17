0ec29f23f21bd9b97224792fac7acb9fd93e5936
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-12-17 21:25:12 +0000

    Implemented logic for live and summary pages

--- src/pages/live.tsx
+++ src/pages/live.tsx
@@ -1,206 +1,379 @@
-import { useEffect, useRef, useState } from "react"
+import { useCallback, useEffect, useReducer, useRef, useState } from "react"
+
+import router from "next/router"
 
 import { TextButton } from "@scientisst/react-ui/components/inputs"
-import { useLiveD3Chart } from "@scientisst/react-ui/hooks"
 import {
 	CHANNEL,
 	COMMUNICATION_MODE,
 	ConnectionFailedException,
 	ConnectionLostException,
 	ScientISST,
-	UserCancelledException
+	ScientISSTFrame,
+	UserCancelledException,
+	framesToUtf16
 } from "@scientisst/sense/future"
 
-import CanvasChart from "../components/charts/CanvasChart"
 import SenseLayout from "../components/layout/SenseLayout"
 
-const p = {
-	className: "w-full h-96",
-	fontFamily: "Lexend"
+enum CONNECTION_STATUS {
+	DISCONNECTED,
+	CONNECTION_LOST,
+	CONNECTION_FAILED,
+	CONNECTING,
+	CONNECTED,
+	ACQUIRING,
+	PAUSED,
+	STOPPING
+}
+
+type LastValues = {
+	[CHANNEL.AI1]: number | undefined
+	[CHANNEL.AI2]: number | undefined
+	[CHANNEL.AI3]: number | undefined
+	[CHANNEL.AI4]: number | undefined
+	[CHANNEL.AI5]: number | undefined
+	[CHANNEL.AI6]: number | undefined
 }
 
 const Page = () => {
 	const scientisstRef = useRef(new ScientISST())
+	const [connectionStatus, setConnectionStatus] = useState(
+		CONNECTION_STATUS.DISCONNECTED
+	)
+	const [activeChannels, setActiveChannels] = useState<CHANNEL[]>([])
+	const [activeSamplingRate, setActiveSamplingRate] = useState(0)
+	const [lastValues, dispatchLastValues] = useReducer(
+		(state: LastValues, action: LastValues) => {
+			return { ...state, ...action }
+		},
+		{
+			[CHANNEL.AI1]: undefined,
+			[CHANNEL.AI2]: undefined,
+			[CHANNEL.AI3]: undefined,
+			[CHANNEL.AI4]: undefined,
+			[CHANNEL.AI5]: undefined,
+			[CHANNEL.AI6]: undefined
+		}
+	)
+	const [segment, setSegment] = useState(1)
+	const frameSequenceRef = useRef(0)
 
-	const [isConnected, setIsConnected] = useState(false)
-	const [isConnecting, setIsConnecting] = useState(false)
-	const [hasConnectionFailed, setHasConnectionFailed] = useState(false)
-	const [isAcquiring, setIsAcquiring] = useState(false)
-	const [hasLostConnection, setHasLostConnection] = useState(false)
+	const bufferRef = useRef<ScientISSTFrame[]>([])
+	const appendBuffer = useCallback(
+		(frames: ScientISSTFrame[], segment: number, bufferLimit: number) => {
+			const newState = [...bufferRef.current, ...frames]
 
-	const [channel1, setChannel1] = useState(0)
-	const [channel2, setChannel2] = useState(0)
-	const [channel3, setChannel3] = useState(0)
+			if (newState.length >= bufferLimit) {
+				const i = newState.length - (newState.length % 16)
+				const newData = framesToUtf16(newState.slice(0, i))
+				const oldData = localStorage.getItem("aq_seg" + segment) ?? ""
 
-	const sequenceRef = useRef(0)
+				localStorage.setItem("aq_seg" + segment, oldData + newData)
+
+				bufferRef.current = newState.slice(i)
+			} else {
+				bufferRef.current = newState
+			}
+		},
+		[]
+	)
+
+	const clearBuffer = useCallback((segment: number) => {
+		const i = bufferRef.current.length - (bufferRef.current.length % 16)
+		const newData = framesToUtf16(bufferRef.current.slice(0, i))
+		const oldData = localStorage.getItem("aq_seg" + segment) ?? ""
+
+		localStorage.setItem("aq_seg" + segment, oldData + newData)
+		bufferRef.current = []
+	}, [])
+
+	const flushBuffer = useCallback(() => {
+		bufferRef.current = []
+	}, [])
 
 	useEffect(() => {
-		if (isAcquiring) {
-			let work = true
+		if (connectionStatus === CONNECTION_STATUS.ACQUIRING) {
+			let valid = true
 
 			new Promise(async resolve => {
-				while (work && scientisstRef.current.isConnected) {
+				while (valid) {
 					try {
-						const data = await scientisstRef.current.readFrames(20)
-						if (data.length === 0) {
+						const frames = await scientisstRef.current.readFrames(
+							Math.ceil(activeSamplingRate / 15)
+						)
+
+						if (frames.length === 0) {
 							break
 						}
-						setChannel1(data[data.length - 1].analog[CHANNEL.AI1])
-						setChannel2(data[data.length - 1].analog[CHANNEL.AI2])
-						setChannel3(data[data.length - 1].analog[CHANNEL.AI3])
 
-						const d: Array<[number, number | null]> = []
-
-						let sequence = sequenceRef.current
-
-						for (let i = 0; i < data.length; i++) {
-							const analog = data[i].analog[CHANNEL.AI1]
-
-							sequence++
-							d.push([sequence, analog])
+						if (valid) {
+							dispatchLastValues(frames[frames.length - 1].analog)
+							appendBuffer(frames, segment, activeSamplingRate)
+						} else {
+							frameSequenceRef.current = 0
 						}
-
-						sequenceRef.current = sequence
-
-						// appendData(d)
-						break
-					} catch (e) {
-						if (e instanceof ConnectionLostException) {
-							if (isAcquiring) {
-								setHasLostConnection(true)
-								setIsAcquiring(false)
-								setIsConnected(false)
+					} catch (error) {
+						if (valid) {
+							if (error instanceof ConnectionLostException) {
+								setConnectionStatus(
+									CONNECTION_STATUS.CONNECTION_LOST
+								)
+							} else {
+								setConnectionStatus(
+									CONNECTION_STATUS.CONNECTION_FAILED
+								)
+								throw error
 							}
-							break
 						}
-						throw e
 					}
 				}
-
 				resolve(undefined)
 			})
 
 			return () => {
-				work = false
+				valid = false
+			}
+		} else if (connectionStatus === CONNECTION_STATUS.PAUSED) {
+			let valid = true
+
+			new Promise(async resolve => {
+				while (valid) {
+					try {
+						const frames = await scientisstRef.current.readFrames(
+							activeSamplingRate
+						)
+
+						if (frames.length === 0 || !valid) {
+							break
+						}
+
+						frameSequenceRef.current += frames.length
+					} catch (error) {
+						if (valid) {
+							if (error instanceof ConnectionLostException) {
+								setConnectionStatus(
+									CONNECTION_STATUS.CONNECTION_LOST
+								)
+							} else {
+								setConnectionStatus(
+									CONNECTION_STATUS.CONNECTION_FAILED
+								)
+								throw error
+							}
+						}
+					}
+				}
+				resolve(undefined)
+			})
+
+			return () => {
+				valid = false
 			}
 		}
-	}, [isAcquiring])
+	}, [
+		activeChannels,
+		activeSamplingRate,
+		appendBuffer,
+		connectionStatus,
+		segment
+	])
+
+	const connect = useCallback(async () => {
+		if (
+			connectionStatus === CONNECTION_STATUS.DISCONNECTED ||
+			connectionStatus === CONNECTION_STATUS.CONNECTION_FAILED ||
+			connectionStatus === CONNECTION_STATUS.CONNECTION_LOST
+		) {
+			setConnectionStatus(CONNECTION_STATUS.CONNECTING)
+
+			const settings = localStorage.getItem("settings") || "{}"
+			const communicationMode =
+				JSON.parse(settings).communication ||
+				COMMUNICATION_MODE.BLUETOOTH
+
+			try {
+				await scientisstRef.current.connect(communicationMode)
+				setConnectionStatus(CONNECTION_STATUS.CONNECTED)
+			} catch (error) {
+				if (error instanceof UserCancelledException) {
+					setConnectionStatus(CONNECTION_STATUS.DISCONNECTED)
+				} else if (error instanceof ConnectionFailedException) {
+					setConnectionStatus(CONNECTION_STATUS.CONNECTION_FAILED)
+				} else {
+					throw error
+				}
+			}
+		}
+	}, [connectionStatus])
+
+	const disconnect = useCallback(async () => {
+		if (
+			connectionStatus === CONNECTION_STATUS.CONNECTED ||
+			connectionStatus === CONNECTION_STATUS.ACQUIRING
+		) {
+			try {
+				if (!scientisstRef.current.isIdle()) {
+					await scientisstRef.current.stop()
+				}
+				await scientisstRef.current.disconnect()
+			} catch {}
+			setConnectionStatus(CONNECTION_STATUS.DISCONNECTED)
+		}
+	}, [connectionStatus])
+
+	const start = useCallback(async () => {
+		if (connectionStatus === CONNECTION_STATUS.CONNECTED) {
+			setConnectionStatus(CONNECTION_STATUS.ACQUIRING)
+
+			// Read channels and sample rate from settings
+			const settings = localStorage.getItem("settings") || "{}"
+			const channels = JSON.parse(settings).channels || [
+				CHANNEL.AI1,
+				CHANNEL.AI2,
+				CHANNEL.AI3,
+				CHANNEL.AI4,
+				CHANNEL.AI5,
+				CHANNEL.AI6
+			]
+			const sampleRate = JSON.parse(settings).sampleRate || 1000
+			scientisstRef.current.getAdcCharacteristics().then(adcChars => {
+				localStorage.setItem("aq_adcChars", adcChars.toJSON())
+			})
+
+			channels.sort()
+
+			setActiveChannels(channels)
+			setActiveSamplingRate(sampleRate)
+			setSegment(1)
+
+			for (const key in localStorage) {
+				if (key.startsWith("aq_")) {
+					localStorage.removeItem(key)
+				}
+			}
+
+			localStorage.setItem("aq_channels", JSON.stringify(channels))
+			localStorage.setItem("aq_sampleRate", JSON.stringify(sampleRate))
+			localStorage.setItem("aq_segments", JSON.stringify(1))
+			localStorage.setItem("aq_gracefullyStopped", JSON.stringify(false))
+
+			flushBuffer()
+
+			try {
+				await scientisstRef.current.start(channels, sampleRate, false)
+			} catch (error) {
+				if (error instanceof ConnectionLostException) {
+					setConnectionStatus(CONNECTION_STATUS.CONNECTION_LOST)
+				} else {
+					setConnectionStatus(CONNECTION_STATUS.CONNECTION_FAILED)
+					throw error
+				}
+			}
+		}
+	}, [connectionStatus, flushBuffer])
+
+	const pauseResume = useCallback(async () => {
+		if (connectionStatus === CONNECTION_STATUS.PAUSED) {
+			setSegment(segment + 1)
+			localStorage.setItem("aq_segments", JSON.stringify(segment + 1))
+			setConnectionStatus(CONNECTION_STATUS.ACQUIRING)
+		} else if (connectionStatus === CONNECTION_STATUS.ACQUIRING) {
+			setConnectionStatus(CONNECTION_STATUS.PAUSED)
+			clearBuffer(segment)
+		}
+	}, [clearBuffer, connectionStatus, segment])
+
+	const stop = useCallback(async () => {
+		if (
+			connectionStatus === CONNECTION_STATUS.ACQUIRING ||
+			connectionStatus === CONNECTION_STATUS.PAUSED
+		) {
+			if (connectionStatus === CONNECTION_STATUS.ACQUIRING) {
+				clearBuffer(segment)
+			}
+
+			setConnectionStatus(CONNECTION_STATUS.STOPPING)
+
+			try {
+				if (!scientisstRef.current.isIdle()) {
+					await scientisstRef.current.stop()
+				}
+				await scientisstRef.current.disconnect()
+				router.push("/summary")
+			} catch (error) {
+				console.error(error)
+			}
+		}
+	}, [clearBuffer, connectionStatus, segment])
 
 	return (
 		<SenseLayout
-			className="container flex flex-col items-center justify-start gap-8 py-8 px-8"
+			className="container flex flex-col items-center justify-start gap-4 p-8"
 			title="Live Acquisition"
 			shortTitle="Live"
 			returnHref="/"
-			style={{
-				minHeight: "calc(100vh - 18.5rem)"
-			}}
 		>
-			{!isConnected ? (
-				<TextButton
-					size="base"
-					onClick={async () => {
-						if (!isConnecting) {
-							setIsConnecting(true)
-							setHasConnectionFailed(false)
-							setHasLostConnection(false)
-
-							try {
-								await scientisstRef.current.connect(
-									COMMUNICATION_MODE.BLUETOOTH
-								)
-								setIsConnected(true)
-								setIsConnecting(false)
-							} catch (error) {
-								if (error instanceof UserCancelledException) {
-									setIsConnecting(false)
-								} else if (
-									error instanceof ConnectionFailedException
-								) {
-									setIsConnecting(false)
-									setHasConnectionFailed(true)
-								}
-							}
-						}
-					}}
-				>
-					Connect
-				</TextButton>
-			) : null}
-			{isConnected ? (
-				<>
+			<div className="flex flex-row gap-4">
+				{(connectionStatus === CONNECTION_STATUS.DISCONNECTED ||
+					connectionStatus === CONNECTION_STATUS.CONNECTING ||
+					connectionStatus === CONNECTION_STATUS.CONNECTION_FAILED ||
+					connectionStatus === CONNECTION_STATUS.CONNECTION_LOST) && (
 					<TextButton
-						size="base"
-						onClick={async () => {
-							if (!isAcquiring) {
-								try {
-									await scientisstRef.current.start(
-										[CHANNEL.AI1, CHANNEL.AI2, CHANNEL.AI3],
-										1000,
-										true
-									)
-									setIsAcquiring(true)
-								} catch (error) {
-									console.log(error)
-									setIsConnected(false)
-								}
-							} else {
-								try {
-									await scientisstRef.current.stop()
-									await scientisstRef.current.disconnect()
-								} catch (error) {
-									console.log(error)
-								}
-								setIsConnected(false)
-								setIsAcquiring(false)
-							}
-						}}
+						size={"base"}
+						onClick={connect}
+						disabled={
+							connectionStatus === CONNECTION_STATUS.CONNECTING
+						}
 					>
-						{isAcquiring ? "Stop" : "Start"} Acquisition
+						Connect
 					</TextButton>
-					{!isAcquiring && (
-						<TextButton
-							size="base"
-							onClick={async () => {
-								try {
-									if (!scientisstRef.current.isIdle()) {
-										await scientisstRef.current.stop()
-									}
-									await scientisstRef.current.disconnect()
-								} catch (error) {
-									console.log(error)
-								}
-								setIsConnected(false)
-							}}
-						>
+				)}
+				{connectionStatus === CONNECTION_STATUS.CONNECTED && (
+					<>
+						<TextButton size={"base"} onClick={start}>
+							Start
+						</TextButton>
+						<TextButton size={"base"} onClick={disconnect}>
 							Disconnect
 						</TextButton>
-					)}
-				</>
-			) : null}
-			{isConnected ? <span>Connected!</span> : null}
-			{isConnecting ? <span>Connecting...</span> : null}
-			{hasConnectionFailed ? <span>Connection lost!</span> : null}
-			{hasConnectionFailed && !isConnected ? (
+					</>
+				)}
+				{(connectionStatus === CONNECTION_STATUS.ACQUIRING ||
+					connectionStatus === CONNECTION_STATUS.PAUSED) && (
+					<>
+						<TextButton size={"base"} onClick={pauseResume}>
+							{connectionStatus === CONNECTION_STATUS.PAUSED
+								? "Resume"
+								: "Pause"}
+						</TextButton>
+						<TextButton size={"base"} onClick={stop}>
+							Stop
+						</TextButton>
+					</>
+				)}
+			</div>
+			{connectionStatus === CONNECTION_STATUS.CONNECTING && (
+				<span>Attempting to connect...</span>
+			)}
+			{connectionStatus === CONNECTION_STATUS.CONNECTION_FAILED && (
 				<span>Connection failed!</span>
-			) : null}
-
-			{isAcquiring ? (
-				<>
-					<span>Channel 1: {channel1}</span>
-					<span>Channel 2: {channel2}</span>
-					<span>Channel 3: {channel3}</span>
-				</>
-			) : null}
-			<CanvasChart
-				className="h-96 w-full"
-				data={[
-					[0, 1],
-					[1, 2],
-					[2, 0]
-				]}
-			/>
-			<div className="h-[20rem] w-full"></div>
+			)}
+			{connectionStatus === CONNECTION_STATUS.CONNECTION_LOST && (
+				<span>Connection lost!</span>
+			)}
+			{connectionStatus === CONNECTION_STATUS.CONNECTION_LOST && (
+				<span>Stopping acquisition...</span>
+			)}
+			{lastValues &&
+				connectionStatus === CONNECTION_STATUS.ACQUIRING &&
+				activeChannels.map(channel => {
+					return (
+						<span key={channel}>{`Channel ${channel}: ${
+							lastValues[channel as number]
+						}`}</span>
+					)
+				})}
 		</SenseLayout>
 	)
 }
---
+++ src/pages/summary.tsx
@@ -0,0 +1,78 @@
+import { TextButton } from "@scientisst/react-ui/components/inputs"
+import {
+	CHANNEL,
+	ScientISSTAdcCharacteristics,
+	utf16ToFrames
+} from "@scientisst/sense/future"
+import FileSaver from "file-saver"
+import JSZip from "jszip"
+
+import SenseLayout from "../components/layout/SenseLayout"
+
+const Page = () => {
+	const convertToCSV = () => {
+		const adcChars = ScientISSTAdcCharacteristics.fromJSON(
+			localStorage.getItem("aq_adcChars")
+		)
+
+		const channels: CHANNEL[] = JSON.parse(
+			localStorage.getItem("aq_channels")
+		)
+
+		const channelNames: string[] = channels.map(channel => CHANNEL[channel])
+
+		const segments: number = JSON.parse(localStorage.getItem("aq_segments"))
+
+		const zip = new JSZip()
+
+		for (let i = 1; i <= segments; i++) {
+			const fileContent = []
+			const frames = utf16ToFrames(
+				localStorage.getItem(`aq_seg${i}`),
+				channels,
+				adcChars
+			)
+
+			// append header
+			fileContent.push("Sequence," + channelNames.join(","))
+
+			// append data
+			for (let j = 0; j < frames.length; j++) {
+				const frameContent = []
+
+				frameContent.push(frames[j].sequence)
+
+				for (let k = 0; k < channels.length; k++) {
+					frameContent.push(frames[j].analog[channels[k]])
+				}
+
+				fileContent.push(frameContent.join(","))
+			}
+
+			zip.file(`segment_${i}.csv`, fileContent.join("\n"))
+		}
+
+		zip.generateAsync({ type: "blob" }).then(content => {
+			FileSaver.saveAs(content, "data.zip")
+		})
+	}
+
+	return (
+		<SenseLayout
+			title="Summary"
+			returnHref="/live"
+			className="flex w-[480px] flex-col items-center justify-center gap-8 py-8 px-8 sm:w-[640px]"
+		>
+			<span>End of acquisition!</span>
+			<TextButton
+				size="base"
+				className="flex-grow"
+				onClick={convertToCSV}
+			>
+				Download as CSV
+			</TextButton>
+		</SenseLayout>
+	)
+}
+
+export default Page
