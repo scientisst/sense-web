75aa08a6d4f2b40b6f4288803194ac837a6b1a57
Rui Maciel <ruigouveiamaciel@gmail.com>
2023-02-09 14:16:53 +0000

    useful logotypes

---
+++ packages/chakra-ui/src/assets/index.ts
@@ -0,0 +1,6 @@
+export * from "./ItLogotype"
+export * from "./TecnicoLogotype"
+export * from "./ScientISSTLogotype"
+export * from "./SenseLogo"
+export * from "./SenseLogotype"
+export * from "./SenseVerticalLogotype"
