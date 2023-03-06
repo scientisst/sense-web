c2a083b8776cb6f99315910c8aa3fca522709758
Rui Maciel <ruigouveiamaciel@gmail.com>
2023-03-06 15:49:42 +0000

    fix attribute typo

--- packages/sense-api/src/future/frame.ts
+++ packages/sense-api/src/future/frame.ts
@@ -2,16 +2,16 @@ import { ScientISSTAdcCharacteristics } from "./adcCharacteristics"
 import { CHANNEL } from "./enums"
 
 export class ScientISSTFrame {
-	public readonly ScientificISSTAdcCharacteristics: ScientISSTAdcCharacteristics
+	public readonly adcCharacteristics: ScientISSTAdcCharacteristics
 	public readonly analog: Record<CHANNEL, number | null | undefined>
 	public readonly sequence: number
 
 	constructor(
-		ScientificISSTAdcCharacteristics: ScientISSTAdcCharacteristics,
+		adcCharacteristics: ScientISSTAdcCharacteristics,
 		analog: Record<CHANNEL, number | null | undefined>,
 		sequence: number
 	) {
-		this.ScientificISSTAdcCharacteristics = ScientificISSTAdcCharacteristics
+		this.adcCharacteristics = adcCharacteristics
 		this.analog = analog
 		this.sequence = sequence
 	}
