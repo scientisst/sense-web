018cd3a2d2fd2b21cb3f1cffe4edccc753f4da70
diogojarodrigues <diogojaordrigues@outlook.pt>
2024-01-22 09:09:34 +0000

    style labels events

--- apps/sense-web-v2/src/components/ShowEvents.tsx
+++ apps/sense-web-v2/src/components/ShowEvents.tsx
@@ -1,11 +1,33 @@
-const EventsLabel = ({eventsLabel}) => {
+import { Box, Flex } from '@chakra-ui/react';
+import React from 'react';
 
-    return (
-        <div>
-          {eventsLabel && Array.isArray(eventsLabel) && eventsLabel.map(event => event.name)}
-        </div>
-      );
-
-}
+const EventsLabel = ({ eventsLabel }) => {
+  return (
+    <Flex>
+      {eventsLabel &&
+        Array.isArray(eventsLabel) &&
+        eventsLabel.map((event, index) => (
+          <Flex
+            key={index}
+            alignItems="center"
+            mr={4}
+            mb={2}
+            fontWeight="bold"
+            color="gray.700"
+          >
+            <Box
+              mr={1}
+              width="10px"
+              height="10px"
+              borderRadius="full"
+              bg={event.color || 'teal.500'}
+            />
+            <Box>{event.name}</Box>
+            <Box ml={1}>({event.key.toUpperCase()})</Box>
+          </Flex>
+        ))}
+    </Flex>
+  );
+};
 
 export default EventsLabel;
